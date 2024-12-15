a65d28729273137670c46f56609a37d7bf622b5e
Bryan Stone <aegixx@gmail.com>
2024-12-14 21:39:32 -0600

    First release
    Signed-off-by: Bryan Stone <aegixx@gmail.com>

--- README.md
+++ README.md
@@ -1,94 +1,71 @@
-# Obsidian Sample Plugin
+# Obsidian Contact Cards Plugin
 
-This is a sample plugin for Obsidian (https://obsidian.md).
+The Obsidian Contact Cards Plugin transforms YAML data inside a special code block into a beautifully designed contact card. This lets you quickly view a person's details (even in preview mode).
 
-This project uses TypeScript to provide type checking and documentation.
-The repo depends on the latest plugin API (obsidian.d.ts) in TypeScript Definition format, which contains TSDoc comments describing what it does.
+## Attribution
 
-This sample plugin demonstrates some of the basic functionality the plugin API can do.
-- Adds a ribbon icon, which shows a Notice when clicked.
-- Adds a command "Open Sample Modal" which opens a Modal.
-- Adds a plugin setting tab to the settings page.
-- Registers a global click event and output 'click' to the console.
-- Registers a global interval which logs 'setInterval' to the console.
+- [Profile Card UI](https://codepen.io/genarocolusso/pen/xONEXg) by [Genaro Colusso](https://codepen.io/genarocolusso)
+- Profile photos from [Gravatar](https://gravatar.com/)
+- Company logos from [Brandfetch](https://brandfetch.com)
+- Vector graphics from [SVG Repo](https://www.svgrepo.com)
 
-## First time developing plugins?
+## Features
 
-Quick starting guide for new plugin devs:
+- **Beautiful Formatting**: Display name, title, company, email, and phone number in a clean, business-card style layout.
+- **Simple Syntax**: Just provide a `contact-card` code block with YAML fields, and the plugin will handle the rest.
+- **Customizable**: Adjust styling by modifying the plugin’s CSS or by creating your own Obsidian CSS snippets.
+- **Powerful Integrations**: Photos automatically populate from Gravatar, Company logos pulled from Brandfetch, and clickable fields for quick access to phone, email, location, LinkedIn and more.
 
-- Check if [someone already developed a plugin for what you want](https://obsidian.md/plugins)! There might be an existing plugin similar enough that you can partner up with.
-- Make a copy of this repo as a template with the "Use this template" button (login to GitHub if you don't see it).
-- Clone your repo to a local development folder. For convenience, you can place this folder in your `.obsidian/plugins/your-plugin-name` folder.
+## Development
+
+- Clone the repo to a local development folder. For convenience, you can place this folder in your `.obsidian/plugins/obsidian-contact-cards` folder.
 - Install NodeJS, then run `npm i` in the command line under your repo folder.
 - Run `npm run dev` to compile your plugin from `main.ts` to `main.js`.
-- Make changes to `main.ts` (or create new `.ts` files). Those changes should be automatically compiled into `main.js`.
 - Reload Obsidian to load the new version of your plugin.
 - Enable plugin in settings window.
 - For updates to the Obsidian API run `npm update` in the command line under your repo folder.
 
-## Releasing new releases
+## Usage
 
-- Update your `manifest.json` with your new version number, such as `1.0.1`, and the minimum Obsidian version required for your latest release.
-- Update your `versions.json` file with `"new-plugin-version": "minimum-obsidian-version"` so older versions of Obsidian can download an older version of your plugin that's compatible.
-- Create new GitHub release using your new version number as the "Tag version". Use the exact version number, don't include a prefix `v`. See here for an example: https://github.com/obsidianmd/obsidian-sample-plugin/releases
-- Upload the files `manifest.json`, `main.js`, `styles.css` as binary attachments. Note: The manifest.json file must be in two places, first the root path of your repository and also in the release.
-- Publish the release.
+1. **Add a code block** to your note:
 
-> You can simplify the version bump process by running `npm version patch`, `npm version minor` or `npm version major` after updating `minAppVersion` manually in `manifest.json`.
-> The command will bump version in `manifest.json` and `package.json`, and add the entry for the new version to `versions.json`
+    ```md
+        ```contact-card
+        name: Jane Doe
+        title: Head of Engineering
+        company: Acme Corp
+        email: jane.doe@example.com
+        phone: 555 123 4567
+        ```
+    ```
 
-## Adding your plugin to the community plugin list
+2. **View the rendered card** in Reading mode.  
+You’ll see a nicely formatted contact card in place of the code block.
 
-- Check the [plugin guidelines](https://docs.obsidian.md/Plugins/Releasing/Plugin+guidelines).
-- Publish an initial version.
-- Make sure you have a `README.md` file in the root of your repo.
-- Make a pull request at https://github.com/obsidianmd/obsidian-releases to add your plugin.
+## Customization
 
-## How to use
+- **CSS Themes**:  
+The plugin uses Obsidian’s built-in CSS variables. If you are using a custom theme, the card styling should adapt automatically.
+- **Further Customization**:  
+You can add custom CSS in your `Snippets` to change the border, font, colors, or layout of the contact card:
 
-- Clone this repo.
-- Make sure your NodeJS is at least v16 (`node --version`).
-- `npm i` or `yarn` to install dependencies.
-- `npm run dev` to start compilation in watch mode.
-
-## Manually installing the plugin
-
-- Copy over `main.js`, `styles.css`, `manifest.json` to your vault `VaultFolder/.obsidian/plugins/your-plugin-id/`.
-
-## Improve code quality with eslint (optional)
-- [ESLint](https://eslint.org/) is a tool that analyzes your code to quickly find problems. You can run ESLint against your plugin to find common bugs and ways to improve your code. 
-- To use eslint with this project, make sure to install eslint from terminal:
-  - `npm install -g eslint`
-- To use eslint to analyze this project use this command:
-  - `eslint main.ts`
-  - eslint will then create a report with suggestions for code improvement by file and line number.
-- If your source code is in a folder, such as `src`, you can use eslint with this command to analyze all files in that folder:
-  - `eslint .\src\`
-
-## Funding URL
-
-You can include funding URLs where people who use your plugin can financially support it.
-
-The simple way is to set the `fundingUrl` field to your link in your `manifest.json` file:
-
-```json
-{
-    "fundingUrl": "https://buymeacoffee.com"
-}
-```
-
-If you have multiple URLs, you can also do:
-
-```json
-{
-    "fundingUrl": {
-        "Buy Me a Coffee": "https://buymeacoffee.com",
-        "GitHub Sponsor": "https://github.com/sponsors",
-        "Patreon": "https://www.patreon.com/"
+    ```css
+    .contact-card-container {
+    border: 2px solid #ff0000;
+    background-color: #f2f2f2;
     }
-}
-```
+    ```
 
-## API Documentation
+## Roadmap
 
-See https://github.com/obsidianmd/obsidian-api
+- Support additional contact fields (e.g., address, social media links).
+- Add configuration options to show/hide specific fields.
+
+## Contributing
+
+Contributions, issues, and feature requests are welcome!  
+Feel free to open an issue or submit a pull request if you find any bugs or have suggestions for improvements.
+
+## License
+
+[MIT](LICENSE)
--- main.ts
+++ main.ts
@@ -1,134 +1,216 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, MarkdownPostProcessorContext, parseYaml, Plugin, PluginSettingTab, Setting, TFile } from 'obsidian';
+import * as crypto from "crypto"
+import { PhoneNumberFormat, PhoneNumberUtil } from 'google-libphonenumber';
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+interface ContactCardsPluginSettings {
+    htmlTemplate?: string;
+    brandfetchClientId?: string;
+    defaultCountryCode: string
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: ContactCardsPluginSettings = {
+    defaultCountryCode: 'US'
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class ContactCardsPlugin extends Plugin {
+    settings: ContactCardsPluginSettings;
 
-	async onload() {
-		await this.loadSettings();
+    async onload() {
+        await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+        // This adds a settings tab so the user can configure various aspects of the plugin
+        this.addSettingTab(new ContactCardsSettingTab(this.app, this));
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+        // Register a post-processor for code blocks of language `contact-card`
+        this.registerMarkdownCodeBlockProcessor("contact-card", (source, el, ctx) =>
+            this.renderContactCard(source, el, ctx)
+        );
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+        window.CodeMirror.defineMode("contact-card", config => window.CodeMirror.getMode(config, "yaml"));
+    }
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
+    onunload() {
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+    }
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
+    async loadSettings() {
+        this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+    }
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
+    async saveSettings() {
+        await this.saveData(this.settings);
+    }
 
-	onunload() {
+    renderError(el: HTMLElement, error: unknown) {
+        let errorMsg = 'Something went wrong';
+        if (error instanceof Error) {
+            errorMsg = `${error.name} - ${error.message}`;
+        } else if (typeof error === 'string') {
+            errorMsg = error;
+        } else if (typeof error === 'object' && error !== null && 'toString' in error && typeof error.toString === 'function') {
+            errorMsg = error.toString();
+        }
+        return el.createDiv({ cls: 'contact-card-error', text: errorMsg });
+    }
 
-	}
+    async renderContactCard(source: string, el: HTMLElement, ctx: MarkdownPostProcessorContext) {
+        try {
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
+            const contactData = parseYaml(source) ?? {
+                name: 'John Doe',
+                title: 'The Everyman',
+                company: 'Acme Inc.',
+                email: 'john.doe@example.com',
+                phone: 5551234567,
+                location: 'Nowhere, OK'
+            };
 
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
+            const container = el.createDiv({ cls: "contact-card-container" });
+            let html = `
+                <div class="contact-card-content">
+                    <div class="contact-card">
+                        <a href="https://www.linkedin.com/search/results/people/?keywords={{name}}">
+                            <img src="{{photo_url}}" class="contact-card-photo" />
+                        </a>
+                        <a href="https://www.linkedin.com/search/results/companies/?keywords={{company}}">
+                            <img src="{{logo_url}}" class="contact-card-logo contact-card-hidden"" />
+                        </a>
+                        <div class="contact-card-info">
+                            <div class="contact-card-name">{{name}}</div>
+                            <div class="contact-card-title contact-card-hidden"">{{title}}</div>
+                            <div class="contact-card-separator">&nbsp;</div>
+                            <div class="contact-card-company contact-card-hidden"">
+                                <svg viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path fill-rule="evenodd" clip-rule="evenodd" d="M14 0H2V16H6V12H10V16H14V0ZM5 3H7V5H5V3ZM7 7H5V9H7V7ZM9 3H11V5H9V3ZM11 7H9V9H11V7Z" fill="#000000"></path> </g></svg>
+                                {{company}}
+                            </div>
+                            <div class="contact-card-email contact-card-hidden"">
+                                <svg viewBox="0 -2.5 20 20" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" fill="#000000"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <title>email [#1572]</title> <desc>Created with Sketch.</desc> <defs> </defs> <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"> <g id="Dribbble-Light-Preview" transform="translate(-340.000000, -922.000000)" fill="#000000"> <g id="icons" transform="translate(56.000000, 160.000000)"> <path d="M294,774.474 L284,765.649 L284,777 L304,777 L304,765.649 L294,774.474 Z M294.001,771.812 L284,762.981 L284,762 L304,762 L304,762.981 L294.001,771.812 Z" id="email-[#1572]"> </path> </g> </g> </g> </g></svg>
+                                <a href="mailto:{{email}}">{{email}}</a>
+                            </div>
+                            <div class="contact-card-phone contact-card-hidden">
+                                <svg viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M1 5V1H7V5L4.5 7.5L8.5 11.5L11 9H15V15H11C5.47715 15 1 10.5228 1 5Z" fill="#000000"></path> </g></svg>
+                                <a href="tel:{{phone}}">{{phone}}</a>
+                            </div>
+                            <div class="contact-card-location contact-card-hidden">
+                                <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 293.334 293.334" xml:space="preserve" fill="#000000"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g> <g> <path style="fill:#010002;" d="M146.667,0C94.903,0,52.946,41.957,52.946,93.721c0,22.322,7.849,42.789,20.891,58.878 c4.204,5.178,11.237,13.331,14.903,18.906c21.109,32.069,48.19,78.643,56.082,116.864c1.354,6.527,2.986,6.641,4.743,0.212 c5.629-20.609,20.228-65.639,50.377-112.757c3.595-5.619,10.884-13.483,15.409-18.379c6.554-7.098,12.009-15.224,16.154-24.084 c5.651-12.086,8.882-25.466,8.882-39.629C240.387,41.962,198.43,0,146.667,0z M146.667,144.358 c-28.892,0-52.313-23.421-52.313-52.313c0-28.887,23.421-52.307,52.313-52.307s52.313,23.421,52.313,52.307 C198.98,120.938,175.559,144.358,146.667,144.358z"></path> <circle style="fill:#010002;" cx="146.667" cy="90.196" r="21.756"></circle> </g> </g> </g></svg>
+                                <a href="https://www.google.com/maps/place/{{location}}">{{location}}</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            `;
+
+            // If they provide their own template, use that
+            if (this.settings.htmlTemplate) {
+                const templateFile = this.app.vault.getAbstractFileByPath(this.settings.htmlTemplate);
+                if (templateFile instanceof TFile) {
+                    const templateContent = await this.app.vault.cachedRead(templateFile);
+                    html = templateContent;
+                } else {
+                    throw new Error(`${this.settings.htmlTemplate} is not a Template file`);
+                }
+            }
+
+            // Always parse images (since the img tag will still try to render)
+            let photoUrl = contactData.photo_url;
+            if (!photoUrl) {
+                // Only use Gravatar if a photo_url was not provided
+                const emailHash = emailDigest(contactData.email ?? '');
+                photoUrl = `https://gravatar.com/avatar/${emailHash}.jpg?s=120&d=mp`;
+            }
+            html = html.replace(/{{photo_url}}/g, photoUrl);
+
+            let logoUrl = contactData.logo_url;
+            if (!logoUrl && contactData.email) {
+                // Only use Brandfetch if a logo_url was not provided
+                const emailDomain = contactData.email.slice(contactData.email.indexOf('@') + 1).toLowerCase();
+                logoUrl = `https://cdn.brandfetch.io/${emailDomain}/w/100/h/100?c=${this.settings.brandfetchClientId}`;
+            }
+            html = html.replace(/{{logo_url}}/g, logoUrl ?? '');
+
+            // Populate variables
+            for (const k in contactData) {
+                switch (k) {
+                    case 'phone': {
+                        const phoneUtil = PhoneNumberUtil.getInstance();
+                        const phoneNum = phoneUtil.parse(contactData.phone.toString(), this.settings.defaultCountryCode);
+                        const regionCode = phoneUtil.getRegionCodeForNumber(phoneNum);
+                        html = html.replace(/{{phone}}/g, phoneUtil.format(phoneNum, regionCode === this.settings.defaultCountryCode ? PhoneNumberFormat.NATIONAL : PhoneNumberFormat.INTERNATIONAL));
+                        break;
+                    }
+
+                    default:
+                        html = html.replace(new RegExp(`{{${k}}}`, 'g'), contactData[k]);
+                }
+            }
+
+            container.innerHTML = html;
+
+            // Unhide all elements that are present
+            for (const k in contactData) {
+                container.find(`.contact-card-${k}`)?.removeClass('contact-card-hidden');
+
+                if (k === 'company') {
+                    container.find('.contact-card-logo')?.removeClass('contact-card-hidden');
+                }
+            }
+
+        } catch (error) {
+            this.renderError(el, error);
+        }
+    }
+
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+class ContactCardsSettingTab extends PluginSettingTab {
+    plugin: ContactCardsPlugin;
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
+    constructor(app: App, plugin: ContactCardsPlugin) {
+        super(app, plugin);
+        this.plugin = plugin;
+    }
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
+    display(): void {
+        const { containerEl } = this;
+
+        containerEl.empty();
+
+        new Setting(containerEl)
+            .setName('HTML Template')
+            .setDesc('Provide a template to use for rendering contact cards.')
+            .addText(text => text
+                .setPlaceholder('Example: templates/Contact')
+                .setValue(this.plugin.settings.htmlTemplate ?? '')
+                .onChange(async (value) => {
+                    this.plugin.settings.htmlTemplate = value;
+                    await this.plugin.saveSettings();
+                }));
+
+        new Setting(containerEl)
+            .setName('Brandfetch Client ID')
+            .setDesc('Provide a Client ID for the Brandfetch API')
+            .addText(text => text
+                .setPlaceholder('Brandfetch Client ID')
+                .setValue(this.plugin.settings.brandfetchClientId ?? '')
+                .onChange(async (value) => {
+                    this.plugin.settings.brandfetchClientId = value;
+                    await this.plugin.saveSettings();
+                }));
+
+        new Setting(containerEl)
+            .setName('Default Country Code')
+            .setDesc('Specify your country code for phone number formatting.')
+            .addText(text => text
+                .setPlaceholder('US')
+                .setValue(this.plugin.settings.defaultCountryCode)
+                .onChange(async (value) => {
+                    this.plugin.settings.defaultCountryCode = value;
+                    await this.plugin.saveSettings();
+                }));
+
+    }
 }
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+function emailDigest(email: string): string {
+    return crypto.createHash('sha256').update(email.trim().toLowerCase()).digest('hex');
 }
